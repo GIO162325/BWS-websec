d6c2f53e1925b6ec57ea2bd7d427e5e3582c11ba
Maja <miljanic.maja0@.com>
2026-03-25 05:50:43 +0100

    jwt signature not verified

--- src/main/java/com/example/websecurity/security/JwtService.java
+++ src/main/java/com/example/websecurity/security/JwtService.java
@@ -57,7 +57,7 @@ public class JwtService {
         return Jwts.parserBuilder()
                 .setSigningKey(getSignInKey())
                 .build()
-                .parseClaimsJws(token)
+                .parseClaimsJwt(token)
                 .getBody();
     }
 
